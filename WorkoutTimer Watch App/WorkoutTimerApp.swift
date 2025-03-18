7f1d2fe1dcd0ba64361f37e91adba118796fda54
xGentuso <ryan.mota@triosstudent.com>
2025-03-18 08:13:20 -0400

    Initial Commit

---
+++ WorkoutTimer Watch App/WorkoutTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTimerApp.swift
+//  WorkoutTimer Watch App
+//
+//  Created by ryan mota on 2025-03-18.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTimer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
